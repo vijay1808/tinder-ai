c307dff6020d05f47ea213de44a2a7a513d60996
vijay <sarathy.hari@gmail.com>
2024-09-10 22:20:45 -0400

    Basic Initializr app in running state

---
+++ src/main/java/io/learning/tinder_ai_backend/TinderAiBackendApplication.java
@@ -0,0 +1,13 @@
+package io.learning.tinder_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAiBackendApplication.class, args);
+	}
+
+}
